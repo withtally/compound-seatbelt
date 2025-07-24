5164699292777fcd6f9bc83ec65f82adf12d8170
Aditya Anand M C <aditya.anandmc@gmail.com>
2025-07-24 12:20:35 +0530

    refactor: remove getSolidityStorageSlotBytes

--- utils/utils.ts
+++ utils/utils.ts
@@ -23,20 +23,6 @@ export function getSolidityStorageSlotUint(
   );
 }
 
-/**
- * @notice Returns the storage slot for a Solidity mapping with bytes32 keys, given the slot of the mapping itself
- * @dev Read more at https://docs.soliditylang.org/en/latest/internals/layout_in_storage.html#mappings-and-dynamic-arrays
- * @param mappingSlot Mapping slot in storage
- * @param key Mapping key to find slot for
- * @returns Storage slot
- */
-export function getSolidityStorageSlotBytes(mappingSlot: `0x${string}`, key: `0x${string}`) {
-  const slot = pad(mappingSlot, { size: 32 });
-  return trim(
-    keccak256(encodeAbiParameters([{ type: 'bytes32' }, { type: 'uint256' }], [key, BigInt(slot)])),
-  );
-}
-
 export function to32ByteHexString(val: bigint | `0x${string}`) {
   const bigIntVal = typeof val === 'string' ? BigInt(val) : val;
   return pad(`0x${bigIntVal.toString(16)}`) as `0x${string}`;
